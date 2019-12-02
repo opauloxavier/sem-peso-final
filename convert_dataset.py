6c7850b0a14a5988da56a6656db0e8e349a2c822
Paulo Xavier <contato.pauloxavier@gmail.com>
2019-12-01 21:46:42 -0300

    Limpando e comentando arquivo de conversão

--- convert_dataset.py
+++ convert_dataset.py
@@ -1,5 +1,5 @@
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 import glob
 import string
 import codecs
@@ -7,8 +7,9 @@ from tqdm import tqdm
 from PIL import Image
 
 path = './dataset/*.png'
-# list files
 files = glob.glob(path)
+
+# itera nos arquivos e cria o dataframe, com '0' nos bytes
 
 
 def contructDataframe(file_list):
@@ -19,14 +20,22 @@ def contructDataframe(file_list):
 
 
 df = contructDataframe(files)
+
+# array que terá o mesmo numero de linhas do dataframe, para substituição da coluna "bytes"
 byteArray = []
 for index, row in tqdm(df.iterrows()):
+    # converte a imagem para escala L e usa como dithering o algoritmo de FLOYDSTEINBERG
     img = Image.open(row["path"]).convert("L", dither=Image.FLOYDSTEINBERG)
+    # transforma os bytes em array
     arr = np.array(img)
+    # faz do array flat
     flat_arr = arr.ravel()
+    # da append na variavel de byteArray
     byteArray.append(flat_arr)
 
+# substitui a coluna "bytes" no df pelos bytes das imagens
 df['bytes'] = byteArray
 
+# dropa a coluna "path" por não ter mais uso e salva um csv localmente
 df.drop(columns=['path']).to_csv(
     'coil.csv', sep=',', index=False, encoding='utf-8')
